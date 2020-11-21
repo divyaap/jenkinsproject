16cbff39c595528834d15f76d9cfc3b19064d4b7
Divya <divyaap2307@gmail.com>
2020-11-21 17:36:55 +0530

    tenth commmit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
    public static void main(String[] args){
-      int x = x/0;
+      int x = 1/0;
       System.out.println("HelloWorld! How are you? Hope you are doing well.");
       System.out.println("Good morning");
       System.out.println("Good afternoon");
